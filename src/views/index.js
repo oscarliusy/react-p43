811c096d2c3845c95521b01f4a18824b26be4c03
oscarliusy <1069176850@qq.com>
2020-02-12 21:35:04 +0800

    p45 react-loadable懒加载

--- src/views/index.js
+++ src/views/index.js
@@ -1,14 +1,49 @@
-import  Dashboard from './Dashboard'
+/*
+*原来是全部加载
+*使用react-loadable变更为按需加载，即懒加载
+*使用Loading高阶组件模拟加载中的等待时间。
+ */
+import Loadable from 'react-loadable'
+//下面是自行重写的懒加载脚本
+//import Loadable from './loadable'
+import { Loading } from '../components'
 
-import  Login from './Login'
+// import  Dashboard from './Dashboard'
+// import  Login from './Login'
+// import  NotFound from './NotFound'
+// import  Settings from './Settings'
+// import  ArticleList from './Article'
+// import  ArticleEdit from './Article/Edit'
 
-import  NotFound from './NotFound'
+const Dashboard = Loadable({
+    loader: () => import('./Dashboard'),
+    loading: Loading,
+})
 
-import  Settings from './Settings'
+const Login = Loadable({
+    loader: () => import('./Login'),
+    loading: Loading,
+})
 
-import  ArticleList from './Article'
+const NotFound = Loadable({
+    loader: () => import('./NotFound'),
+    loading: Loading,
+})
 
-import  ArticleEdit from './Article/Edit'
+const Settings = Loadable({
+    loader: () => import('./Settings'),
+    loading: Loading,
+})
+
+const ArticleList = Loadable({
+    loader: () => import('./Article'),
+    loading: Loading,
+})
+
+const ArticleEdit = Loadable({
+    loader: () => import('./Article/Edit'),
+    loading: Loading,
+})
 
 export {
 
